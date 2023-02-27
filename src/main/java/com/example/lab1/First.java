b2da721230e05086966d8121d8846351dedd2cb0
Максим Доронкин <maksim@MacBook-Pro-Maksim.local>
2023-02-27 21:23:00 +0300

    Add readme.md

--- src/main/java/com/example/lab1/First.java
+++ src/main/java/com/example/lab1/First.java
@@ -11,7 +11,7 @@ public class First extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(First.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 380, 380);
+        Scene scene = new Scene(fxmlLoader.load(), 400, 550);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/lab1/module/Mouse.java
+++
@@ -1,5 +0,0 @@
-package com.example.lab1.module;
-
-public class Mouse {
-
-}
